42b7c477d058f544f4e0d597282586c4f625bc48
Dat Nguyen <hoangdat77ct@gmail.com>
2022-01-14 15:43:08 +0700

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
+
 app.config.from_object('config')
 
 from . import auth
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 SECRET_KEY = 'thisissecret'
 JSON_AS_ASCII = False
 DEBUG = True
+CORS_HEADERS= 'Content-Type'
